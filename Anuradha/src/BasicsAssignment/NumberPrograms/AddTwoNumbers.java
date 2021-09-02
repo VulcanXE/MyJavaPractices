287ab92c7c158193ddb0219a6b94ca6be7cf673b
AnuradhaKaruturi <anuradhakaruturi.ak@gmail.com>
2021-09-02 13:21:12 -0600

    changing folders

--- Anuradha/src/BasicsAssignment/NumberPrograms/AddTwoNumbers.java
+++ Anuradha/src/BasicsAssignment/NumberPrograms/AddTwoNumbers.java
@@ -1,18 +1,23 @@
 package BasicsAssignment.NumberPrograms;
 
 import java.util.*;
-public class AddTwoNumbers {
-    public static void main(String[] args)
+public class AddTwoNumbers { // Class Declaration
+    public static void main(String[] args) //Main method Declaration
     {
-        Scanner scannerObj = new Scanner(System.in);
+        int sum; //variable declaration
+        Scanner scannerObj = new Scanner(System.in); //Object Declaration
         System.out.println("Enter values for A and B");
-        int a = AddTwoNumbers.validate(scannerObj);
-        int b = scannerObj.nextInt();
-        System.out.println("SUM OF A and B is "+(a + b));
-        scannerObj.close();
+        int a = AddTwoNumbers.validate(scannerObj); //Variable initialization along with validation
+        int b = AddTwoNumbers.validate(scannerObj); //validate method call
+        sum = add(a,b); //function call
+        System.out.println("SUM OF A and B is "+ sum);
+        scannerObj.close(); //OBject closing
     }
-
-    static int validate(Scanner scannerObj){
+  static int add(int a ,int b) //add method definition
+ {
+   return a+b; //return statement
+ }
+    static int validate(Scanner scannerObj){ //validate method definition
         int num;
         do{
             System.out.println("Enter positive Number:");
