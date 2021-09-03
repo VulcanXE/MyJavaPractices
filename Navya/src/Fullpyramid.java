a5f0ef9179e9c8fdd11af2ca1037db8e38625f4f
Navya-06 <chowdarynavya1999@gmail.com>
2021-09-03 17:13:41 -0400

    Pyramid commit

--- Navya/src/Fullpyramid.java
+++ Navya/src/Fullpyramid.java
@@ -1,14 +1,22 @@
 public class Fullpyramid {
-     public static void main(String[] args) {
-         int rows = 5; int k = 0;
-         for(int i = 1; i <= rows; ++i){
-             for( int j = 1; j >= i -1; ++j){
-                 System.out.print( " ");
-             }
-             while (k != 2 * i -1){ 
-                 System.out.print("*");
-             }
-             System.out.println();
-         }
-     }
+ public static void main(String[] args)  {
+     int n = 5;
+        for (int i=0; i < n; i++)
+        {
+            for (int j = n-i; j>1; j--)
+            {
+                System.out.print(" ");
+            }
+  
+            
+            for (int j=0; j<=i; j++ )
+            {
+                
+                System.out.print(i+ " "+j);
+            }
+  
+            System.out.println();
+        }
+    }
+    
 }
