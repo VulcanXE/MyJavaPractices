35cbc5f800d7984b6e19651b2f6c310fd863caad
sindhupsvn <sindhuchowdary813@gmaail.com>
2021-09-03 16:08:26 +0530

    commit scanint

--- sindhu/src/ScanInt.java
+++ sindhu/src/ScanInt.java
@@ -1,12 +1,18 @@
 import java.util.Scanner;
-public class ScanInt {
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter an integer: ");
-        int data = input.nextInt();
-        System.out.println("The integer value is: " + data);
-        input.close();
 
-    }
-    
+class Main {
+  public static void main(String[] args) {
+
+    // creates a Scanner object
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Enter an integer: ");
+
+    // reads an int value
+    int data1 = input.nextInt();
+
+    System.out.println("Using nextInt(): " + data1);
+
+    input.close();
+  }
 }
