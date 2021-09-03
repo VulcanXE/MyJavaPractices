96d6de771456a5e8bc48cbc5ea1e46b1958acbc0
sindhupsvn <sindhuchowdary813@gmaail.com>
2021-09-03 06:54:21 +0530

    Integer commit

---
+++ sindhu/src/Intnum.java
@@ -0,0 +1,9 @@
+public class Intnum{
+    public static void main(String[] args) {
+        int x=20;
+        double y = 30d;
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+}
