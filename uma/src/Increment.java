1f9cfdf21bdd1c3cd396321f090d0674697f2d8c
umaboppana <mrsumaram@gmail.com>
2021-09-04 11:19:22 -0500

    uma's first practices

---
+++ uma/src/Increment.java
@@ -0,0 +1,18 @@
+public class Increment {
+    public static void main(String[] args) {
+        int a =10;
+       System.out.println(a-- + a--);
+        int b=10;
+        System.out.println(--b + --b);
+        int c=10;
+        System.out.println(c++ + c++);
+         int d=10;
+        System.out.println(++d + ++d);
+        int e=10;
+        System.out.println(e++ + ++e);
+        int a1=10;
+        System.out.println(a1++ + --a1);
+        
+    }
+    
+}
