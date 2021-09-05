5153e7739d518aaee12ee8772423ab5897d7a41f
Tulasikakara <tulasi.polina@gmail.com>
2021-09-04 23:43:07 -0400

    changes done

--- Tulasi-Java/src/Student.java
+++ Tulasi-Java/src/Student.java
@@ -1,27 +1,25 @@
-import java.util.Scanner;
 public class Student {
+  String name;
+  int roll_no;
+  long phone_number;
+  String address;
 
-    int roll_no = 2;
-    String name;
-    long phone_no;
     public static void main(String[] args) {
+        Student obj=new Student();
+        obj.name="John";
+        obj.roll_no=2;
+        obj.phone_number=9124578963L;
+        obj.address="India";
+        System.out.println("Name is " + obj.name +" and roll number is "+ obj.roll_no +". " + "Phone number -" + obj.phone_number+ " and his address -" +obj.address);
 
-        Student stu = new Student();
-        Student stu1 = new Student();
-        Scanner sc = new Scanner(System.in);
-        Scanner sc1 = new Scanner(System.in);
-        System.out.println("Enter name 1");
-        stu.name = sc.nextLine();
-        stu.roll_no=1;
-        System.out.println("Enter phone number");
-        stu.phone_no = sc.nextLong();
-        System.out.println("Enter name 2");
-        stu1.name=sc1.nextLine();
-        stu1.roll_no=2;
-        System.out.println("Enter phone number");
-        stu1.phone_no = sc1.nextLong();
-        System.out.println(stu.roll_no+ " "+stu.name+ " "+stu.phone_no);
-        System.out.println(stu1.roll_no+ " "+stu1.name+ " "+stu1.phone_no);
+        Student obj1=new Student();
+        obj1.name="Sam";
+        obj1.roll_no=1;
+        obj1.phone_number=3121452363L;
+        obj1.address="USA";
+        System.out.println("Name is " + obj1.name +" and roll number is "+ obj1.roll_no +". " + "Phone number -" + obj1.phone_number+ " and his address -" +obj1.address);
 
     }
+
+
 }
