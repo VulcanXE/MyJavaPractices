85a44914aee33fb3ed8c92917aa24b3b80b7f60b
umaboppana <mrsumaram@gmail.com>
2021-09-04 20:38:56 -0500

    loops practice

--- uma/src/Increment.java
+++ uma/src/Increment.java
@@ -1,6 +1,6 @@
 public class Increment {
     public static void main(String[] args) {
-        int a =10;
+       int a =10;
        System.out.println(a-- + a--);
         int b=10;
         System.out.println(--b + --b);
@@ -10,9 +10,13 @@ public class Increment {
         System.out.println(++d + ++d);
         int e=10;
         System.out.println(e++ + ++e);
-        int a1=10;
-        System.out.println(a1++ + --a1);
-        
+        int i=10;
+        System.out.println(i++ + --i);
+        int j=10;
+        j++;
+        System.out.println(j);
+        --j;
+        System.out.println(j);
     }
     
 }
---
+++ uma/src/Loop.java
@@ -0,0 +1,12 @@
+public class Loop {
+    static int j;
+    public static void main(String[] args) {
+       // int i=1;
+       // for(i=1;i<=10;i++);
+        //System.out.println(i);
+       // while(i<=10){
+         //   System.out.println(i);i++;
+System.out.println(j);
+        }
+    }
+
