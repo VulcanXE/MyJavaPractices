817709fd95db66ab24e653517b8fce20832f060f
sindhupsvn <sindhuchowdary813@gmaail.com>
2021-09-07 16:12:56 +0530

    opeators

---
+++ sindhu/src/Operators.java
@@ -0,0 +1,18 @@
+public class Operators {
+    public static void main(String[] args){
+        int x=9;
+        System.out.println(x++ + x++ + ++x + x++ + ++x + x++ + ++x);
+        System.out.println(-x);
+        int a=10;
+        int b=3;
+        System.out.println(10<<3);
+        System.out.println(10>>3);
+        System.out.println(a>b && a>b);
+        System.out.println(a<b | a>b);
+        System.out.println(~b);
+
+
+
+    }
+    
+}
