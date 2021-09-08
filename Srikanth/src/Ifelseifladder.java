810ae81e889d4461888ef045992d40378e86384c
ysrikanth3460 <ysrikanth3460@gmail.com>
2021-09-08 08:26:40 +0530

    commit

--- Srikanth/src/Ifelseifladder.java
+++ Srikanth/src/Ifelseifladder.java
@@ -4,7 +4,7 @@ public class Ifelseifladder {
         int number = 75;
         if (number <= 15) {
             System.out.println("Fail");
-        } else if (number <= 45) {
+        } else if (number <= 40) {
             System.out.println("Close to pass marks, but failed");
         } else if (number <= 50) {
             System.out.println("you have passed the exam");
