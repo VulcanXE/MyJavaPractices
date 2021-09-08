c565a2660814fc87b2b8ec6e1f4c3e6a23d03486
ysrikanth3460 <ysrikanth3460@gmail.com>
2021-09-08 08:06:36 +0530

    commit

--- Srikanth/src/Ifelse.java
+++ Srikanth/src/Ifelse.java
@@ -1,8 +1,8 @@
 public class Ifelse {
 
     public static void main(String[] args) {
-        int number = 10;
-        if (number / 2 == 0) {
+        int number = 87;
+        if (number % 2 == 0) {
             System.out.println("even numbers");
         } else {
             System.out.println("odd numbers");
