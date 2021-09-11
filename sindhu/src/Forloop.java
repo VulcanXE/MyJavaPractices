c05ba088429734f68ed40fe5ad82f87f3029cafd
sindhupsvn <sindhuchowdary813@gmaail.com>
2021-09-11 07:47:08 +0530

    added nested for loop

--- sindhu/src/Forloop.java
+++ sindhu/src/Forloop.java
@@ -1,4 +1,4 @@
-public class Forloop{
+/*public class Forloop{
     public static void main (String[] args){
         for(int i=5; i>=1; i--){
             for(int j=1; j<=i; j++){
@@ -8,5 +8,29 @@ public class Forloop{
         }
         System.out.println("Out of for");
         
+    }
+}*/
+
+
+
+/*public class Forloop{
+    public static void main (String[] args){
+        int i;
+        int x=5;
+        for(i=0;i<x;i++){
+            System.out.println("the value is"+i" " +i,);
+        }
+
+    }
+}*/
+
+//Nested for loop
+public class Forloop{
+    public static void main(String[] args) {
+        for(int i=1;i<=3;i++){
+            for(int j=1;j<=3;j++){
+                System.out.println(i+ " " +j);
+            }
+        }
     }
 }
