6ccc3de6e2dd94d113e0d5a0b80ad3b652eeb73d
AnuradhaKaruturi <anuradhakaruturi.ak@gmail.com>
2021-09-12 23:38:01 -0600

    package and encapsulation practice

--- Anuradha/src/practice/NestedClasses/TopLevelClass.java
+++ Anuradha/src/practice/NestedClasses/TopLevelClass.java
@@ -1,5 +1,10 @@
 package NestedClasses;
 public class TopLevelClass {
+    public static void main(String[] args) {
+        //package examples
+        OuterClass outobj = new OuterClass();//can create obj for class of another file within this package
+      //  EncapsulationTest encaptestObj = new EncapsulationTest();  cannot create obj for class from another package
+      }
 
     void accessMembers(OuterClass outer) {     
         
